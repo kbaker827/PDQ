90354e465ec5ab6d291e1267a0f9a26a94a1cadd
Kyle Baker <36923376+kbaker827@users.noreply.github.com>
2019-06-28 11:00:35 -0400

    Add files via upload

---
+++ mdt-pdq-psexec.ps1
@@ -0,0 +1,53 @@
+# Get the ID and security principal of the current user account
+$myWindowsID = [System.Security.Principal.WindowsIdentity]::GetCurrent();
+$myWindowsPrincipal = New-Object System.Security.Principal.WindowsPrincipal($myWindowsID);
+
+# Get the security principal for the administrator role
+$adminRole = [System.Security.Principal.WindowsBuiltInRole]::Administrator;
+
+# Check to see if we are currently running as an administrator
+if ($myWindowsPrincipal.IsInRole($adminRole))
+{
+    # We are running as an administrator, so change the title and background colour to indicate this
+    $Host.UI.RawUI.WindowTitle = $myInvocation.MyCommand.Definition + "(Elevated)";
+    $Host.UI.RawUI.BackgroundColor = "DarkBlue";
+    Clear-Host;
+}
+else {
+    # We are not running as an administrator, so relaunch as administrator
+
+    # Create a new process object that starts PowerShell
+    $newProcess = New-Object System.Diagnostics.ProcessStartInfo "PowerShell";
+
+    # Specify the current script path and name as a parameter with added scope and support for scripts with spaces in it's path
+    $newProcess.Arguments = "& '" + $script:MyInvocation.MyCommand.Path + "'"
+
+    # Indicate that the process should be elevated
+    $newProcess.Verb = "runas";
+
+    # Start the new process
+    [System.Diagnostics.Process]::Start($newProcess);
+
+    # Exit from the current, unelevated, process
+    Exit;
+}
+
+# Run your code that needs to be elevated here...
+
+$mdtscripts = '\\hq-mdt-01\MDT\ONWASA_DS\Scripts'
+
+Copy-Item -Path "$mdtscripts\PsExec.exe" -Destination "${Env:windir}" -Verbose
+
+netsh advfirewall set allprofiles state off
+ipconfig /registerdns
+
+psexec.exe \\hq-pdq-01.onwasa.com -h -accepteula ipconfig /flushdns
+psexec.exe \\hq-pdq-01.onwasa.com -h -accepteula pdqdeploy.exe Deploy -Package "MDT Defaults" -Targets $env:COMPUTERNAME
+
+start-sleep 30
+while (test-path "C:\Windows\AdminArsenal\PDQDeployRunner\service-1.lock") {
+    start-sleep 30
+}
+
+Write-Host -NoNewLine "Press any key to continue...";
+$null = $Host.UI.RawUI.ReadKey("NoEcho,IncludeKeyDown");
